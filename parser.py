d0fc9c5684ce8216bf3b8bd2127d924cb49f5ecd
xin3c <57803964+xin3c@users.noreply.github.com>
2020-09-12 23:14:38 +0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 from twitchobserver import Observer
 
-newf = open("new.txt", 'w+')
-good_accounts = []
-channel = 'in3c'
+rfile = input('Print the filename of yor accounts list.\n')
+
+channel = 'nasa'
 
 
 def parse(usr):
@@ -17,18 +17,21 @@ def parse_oau(usr):
     return oauth
 
 
-f = open("text.txt", 'r+')
+try:
+    f = open(rfile, 'r+')
+except FileNotFoundError:
+    print('Wrong filename')
+    exit()
 for i in f.readlines():
     user, oauth = parse(i), parse_oau(i)
     try:
-        with Observer(user, oauth) as observer:
+        with Observer(user, oauth) as observer: 
             observer.join_channel(channel)
-            print(f'[+] {i} ')
-            good_accounts.append(i)
+            newf = open("output.txt", 'a+')
+            print(f'[+] {i}')
+            newf.write(i)
+            newf.close()
+
     except RuntimeError:
-
+        print('[-]' + i)
         continue
-
-newf = open("new.txt", 'w')
-newf.writelines(good_accounts)
-newf.close()
